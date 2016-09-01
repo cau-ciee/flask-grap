7a62b97d40014463e381e7664c60eda89135fc6b
letfly <letflykid@gmail.com>
2016-09-01 10:18:23 +0800

    basic index

---
+++ price/price.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from werkzeug.utils import import_string
+
+blueprints = [
+        'price.views.public:blueprint'
+]
+def create_app():
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+def register_blueprints(app):
+    for bp in blueprints:
+        app.register_blueprint(import_string(bp))
